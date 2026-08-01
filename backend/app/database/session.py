10b6b1aff3afd358a8374562b319186a96ef91d1
Rohit Sonar <rohit456sonar@gmail.com>
2026-08-02 01:39:31 +0530

    models __init__.py adding imports

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -1,7 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from app.core.config import settings
 
-print("DATABASE_URL =", settings.database_url)
-engine = create_engine(settings.database_url,echo=settings.debug,)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Register all models
+import app.models
+
+engine = create_engine(
+    settings.database_url,
+    echo=settings.debug,
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
