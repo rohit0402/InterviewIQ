6d96d76e3641acb57ab4a8527d4caf28cc48683a
Rohit Sonar <rohit456sonar@gmail.com>
2026-08-05 23:34:35 +0530

    changes in frontend for refreshing bugs

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,9 +2,9 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function ProtectedRoute() {
-    const { accessToken, user,intialized } = useSelector((state) => state.auth);
+    const { accessToken, user,initialized } = useSelector((state) => state.auth);
 
-    if (!intialized) {
+    if (!initialized) {
         return <div>Loading...</div>;
     }
 
